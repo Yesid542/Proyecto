5dd4456787f28772d122058abcb8e15c99367753
Yesid Barreto <117132798+Yesid542@users.noreply.github.com>
2025-08-24 20:06:14 -0500

    Add files via upload

---
+++ data/config.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    "db" => [
+        "host" => "localhost",
+        "user" => "root",
+        "pass" => "",
+        "name"  => "wiigoinventario",
+        "port" =>"3306",
+        "options" => [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+    ]   
+];
+
+?>
+
